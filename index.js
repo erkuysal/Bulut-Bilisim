c7704dfede91ded086bee0467a729e6b7c79a594
Feyzullah <f.erkuysal@gmail.com>
2025-04-17 15:18:44 +0300

    ++

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express()
+const port = 3005
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+app.get('/login', (req, res) => {
+    var username = req.query.username
+    var password = req.query.password
+
+    req.
+    // res.send('name: ' + req.query.name);
+    // res.send('password: ' + req.query.password);
+    res.send({
+        'username' : username,
+        'password' : password,
+    })
+})
+
+
+app.get('/image/:width/:height', async (req, res) => {
+    var cevap = await fotoyap(req.params.width, req.params.height);
+    res.setHeader("Content-Type", cevap.header)
+    res.send(cevap.image)
+});
+
+app.get('/image', async (req, res) => {
+    res.send("Bu Degil...")
+});
+
+app.post('/image', async (req, res) => {
+    var a = req.data
+    var b = req.body
+    var cevap = await fotoyap(req.body.width, req.body.height);
+    res.setHeader("Content-Type", cevap.header)
+    res.send(cevap.image)
+});
+
+app.post('/image/:width/:height', async (req, res) => {
+    var cevap = await fotoyap(req.params.width, req.params.height);
+    res.setHeader("Content-Type", cevap.header)
+    res.send(cevap.image)
+});
+
+
+
+async function fotoyap(width,height){
+    const imageUrl = `https://placehold.co/${width}x${height}`;
+
+    try {
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer'});
+        var cevap = {
+            image: response.data,
+            header: response.headers['content-type'],
+        }
+        return cevap
+    }
+    catch (error) {
+        console.error('Error fetching the image:', error);
+    }
+}
+
+
+
